62dd65cd23b615698f2617c2303cad8c9016e36c
Barry <chiroptical@gmail.com>
2022-02-25 13:18:02 -0500

    allow broken nix packages for hasktorch-ffi-th-0.0.1.0

--- flake.nix
+++ flake.nix
@@ -10,7 +10,10 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          allowBroken = true;
+        };
         homingPigeon = import ./homing-pigeon.nix pkgs;
         
       in rec {
