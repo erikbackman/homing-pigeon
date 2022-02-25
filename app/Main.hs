8c23e9840cc795d94a9362fd6ad6b739b4ddf6a4
Barry <chiroptical@gmail.com>
2022-02-25 14:53:09 -0500

    Remove final references to arrayfire

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,8 @@
 
 module Main where
 
-import qualified ArrayFire as A
 import Control.Exception (catch)
 import HomingPigeon
 
 main :: IO ()
-main = print newArray `catch` (\(e :: A.AFException) -> print e)
-  where
-    newArray = A.matrix @Double (2, 2) [[1 ..], [1 ..]] * A.matrix @Double (2, 2) [[2 ..], [2 ..]]
+main = pure ()
