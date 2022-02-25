950f9b30b90a001448201b7b88cf72db72d4d6f6
Barry Moore <chiroptical@gmail.com>
2022-02-25 08:15:45 -0500

    Add iohk flake.nix example

--- flake.nix
+++ flake.nix
@@ -1,44 +1,44 @@
 {
   description = "Homing Pigeon";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs-channels/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    # hasktorch.url = "github:hasktorch/hasktorch";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        compiler = "ghc8107";
-
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        haskellPackages = pkgs.haskellPackages;
-
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-
-        packageName = "homing-pigeon";
-      in
-      {
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
-            # Dependency overrides go here
-          };
-
-        defaultPackage = self.packages.${system}.${packageName};
-
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            haskellPackages.haskell-language-server
-            haskellPackages.fourmolu
-            haskellPackages.hpack
-            haskellPackages.ghcid
-            haskellPackages.cabal-install
-            pkgs.nixpkgs-fmt
-          ];
-          inputsFrom = builtins.attrValues self.packages.${system};
-        };
-      });
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.hasktorch.url = "https://github.com/hasktorch/hasktorch";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          homingPigeonProject =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc8107";
+              # This is used by `nix develop .` to open a shell for use with
+              # `cabal`, `hlint` and `haskell-language-server`
+              shell.tools = {
+                cabal = {};
+                hlint = {};
+                haskell-language-server = {};
+                fourmolu = {};
+                hpack = {};
+              };
+              # Non-Haskell shell tools go here
+              shell.buildInputs = with pkgs; [
+                nixpkgs-fmt
+              ];
+              # This adds `js-unknown-ghcjs-cabal` to the shell.
+              # shell.crossPlatform = p: [p.ghcjs];
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.homingPigeonProject.flake {
+        # This adds support for `nix build .#js-unknown-ghcjs-cabal:hello:exe:hello`
+        # crossPlatforms = p: [p.ghcjs];
+      };
+    in flake // {
+      # Built by `nix build .`
+      defaultPackage = flake.packages."homing-pigeon-exe";
+    });
 }
